284ba62d3ab09df787677cc7b55cba07dee65679
Michael Vilain <michael@vilain.com>
2021-09-07 08:17:19 -0700

    change minikube to use default cni; incl awx-operator.yaml; discuss issues with minikube

--- awx-demo.sh
+++ awx-demo.sh
@@ -31,9 +31,9 @@ minikube version
 ## and the README.md page from https://github.com/ansible/awx-operator.git
 ##
 ## start a local minikube cluster and point minikube to it (other contexts might use docker's minikube)
-minikube start --addons=ingress --cpus=4 --cni=flannel --install-addons=true \
+minikube start --addons=ingress  --install-addons=true \
     --kubernetes-version=stable --memory=12g
-minikube status
+minikube kubectl -- status
 minikube addons list
 
 ## point kubectl to minikube's cluster
@@ -42,9 +42,10 @@ kubectl config view
 kubectl cluster-info
 
 ## install and deploy the awx-operator CRD (Custom Resource Definition) from git repo
-kubectl apply -f https://raw.githubusercontent.com/ansible/awx-operator/0.13.0/deploy/awx-operator.yaml
+## from https://raw.githubusercontent.com/ansible/awx-operator/0.13.0/deploy/awx-operator.yaml
+kubectl apply -f awx-operator.yaml
 
-kubeclt describe deployment
+kubectl describe deployment
 kubectl get pods -w
 
 ## now deploy a simple nodeport instance of AWX
@@ -53,7 +54,14 @@ kubectl get pods -w
 # kubectl get secret awx-demo-admin-password -o jsonpath="{.data.password}" | base64 --decode
 
 ## use nginx for ingress controller
-kubectl apply -f awx-nginx.yml
+kubectl apply -f awx-nginx-ingress.yml
+
+# if you watch the log with
+# kubectl logs -f deployments/awx-operator
+# ansible will report a failure on line 20 of this file:
+# https://github.com/ansible/awx-operator/blob/devel/roles/installer/tasks/resources_configuration.yml
+# and the awx-nginx pod fails to deploy. The awx-nginx-postgres pod is fine.
+
 kubectl kubectl get pods -l "app.kubernetes.io/managed-by=awx-operator" -w
 kubectl get secret awx-demo-admin-password -o jsonpath="{.data.password}" | base64 --decode
 kubectl get nodes -o wide
