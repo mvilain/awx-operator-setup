ff6cc540d3356fbb63aa74911a44eb80e7e12c5c
Michael Vilain <michael@vilain.com>
2021-09-06 19:45:44 -0700

    change comment format; move stuff not relevent to awx-operator to README; remove awx namespace; remove trailing spaces

--- awx-demo.sh
+++ awx-demo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# awx-demo.sh -- setup and demo a kubernetes cluster running awx
-# done in minimum 4 core 16GB centos8 laptop
-
+## awx-demo.sh -- setup and demo a kubernetes cluster running awx
+## done in minimum 4 core 16GB centos8 laptop
+## this is the docker image repository where the awx-operator instance is to be deployed
+## it's currently set to use an ephemeral repository tt.sh and will keep it for 1 hour
+## (DO NOT USE THIS FOR PRODUCTION images)
+IMG_REPO=tt.sh:awx-operator-example:1h
 
 yum install -y git make yum-utils epel-release
 yum install -y ansible
-# yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-# yum install -y docker-ce docker-ce-cli containerd.io
-# systemctl start docker
-# systemctl enable docker
-# curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-# chmod 755 /usr/local/bin/docker-compose
+#yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#yum install -y docker-ce docker-ce-cli containerd.io
+#systemctl start docker
+#systemctl enable docker
+#curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#chmod 755 /usr/local/bin/docker-compose
 
 sudo yum module -y install virt
 sudo yum install -y virt-install virt-viewer conntrack
-
-# [logout]
-# [login]
 
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo chmod 755 kubectl
@@ -29,13 +29,15 @@ sudo curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-li
 chmod 755 /usr/local/bin/minikube
 minikube version
 
-wget https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz
-tar -xzf helm-v3.6.3-linux-amd64.tar.gz
-sudo chmod 755 linux-amd64/helm
-sudo chown root linux-amd64/helm
-sudo mv linux-amd64/helm /usr/local/bin/
-helm version
+##--------------------------------------------------------------------------------
 
+## a lot of this came from this episode of Jeff Geerling's Kubernetes 101 course, episode 7
+git clone https://github.com/ansible/awx-operator.git
+cd awx-operator
+## create awx-operator container and push into tt.sh ephemeral registry for 1h
+make docker-build docker-push  IMG=${IMG_REPO}
+
+## start a local minikube cluster and point minikube to it (other contexts might use docker's minikube)
 minikube start --addons=ingress --cpus=4 --cni=flannel --install-addons=true \
     --kubernetes-version=stable --memory=12g
 minikube status
@@ -45,46 +47,50 @@ eval $(minikube -p minikube docker-env) # point kubectl to minikube's cluster
 kubectl config view
 kubectl cluster-info
 
-kubectl get nodes -o wide
-kubectl get pods -A
+## install and deploy the awx-operator CRD (Custom Resource Definition)
+make install
+make deploy IMG=${IMG_REPO}
 
-kubectl apply -f https://raw.githubusercontent.com/ansible/awx-operator/0.13.0/deploy/awx-operator.yaml
 kubeclt describe deployment
 watch kubectl get pods
 
-kubectl create namespace awx
-# kubectl config set-context --current --namespace awx
-# kubectl config set-context --current --namespace ""
+## now deploy an instance of AWX
 
 kubectl apply -f awx-demo.yml
-watch kubectl get pods -n awx
-# kubectl logs -f deployments/awx-operator
+watch kubectl get pods
+#kubectl logs -f deployments/awx-operator
 
 kubectl get secret awx-demo-admin-password -o jsonpath="{.data.password}" -n awx| base64 --decode
 
-# use nginx for ingress controller
+## use nginx for ingress controller
 kubectl apply -f awx-nginx.yml
-watch kubectl get pods -n awx -l "app.kubernetes.io/managed-by=awx-operator" # -w
-kubectl get ing -n aws #ingress
+watch kubectl get pods -l "app.kubernetes.io/managed-by=awx-operator" # -w
+kubectl get ing # or ingress
 
-# monitor metrics required for HPA (Horizontal Pod Autoscaler)
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install -f values/metrics-server.yml metrics-server bitnami/metrics-server
-kubectl top nodes -n aws
+## monitor metrics required for HPA (Horizontal Pod Autoscaler)
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm install -f values/metrics-server.yml metrics-server bitnami/metrics-server
+#kubectl top nodes
 
-# https://httpd.apache.org/docs/2.4/programs/ab.html
-# apache bench is installed on MacOS by default but needs httpd-tools for centos
-yum install -y httpd-tools
+## https://httpd.apache.org/docs/2.4/programs/ab.html
+## apache bench is installed on MacOS by default but needs httpd-tools for centos
+#yum install -y httpd-tools
 
-# external DNS provider to tie k8s cluster to external DNS provider
-# https://github.com/kubernetes-sigs/external-dns
-# cert-manager uses Let's Encrypt which can't authenticate in many ISP's NAT environments
-kubectl create namespace cert-manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.crds.yaml
-helm repo add jetstack https://charts.jetstack.io
-helm install cert-manager jetstack/cert-manager -n cert-manager --version v1.5.3
+## external DNS provider to tie k8s cluster to external DNS provider
+## https://github.com/kubernetes-sigs/external-dns
+## cert-manager uses Let's Encrypt which can't authenticate in many ISP's NAT environments
+#kubectl create namespace cert-manager
+#kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.crds.yaml
+#helm repo add jetstack https://charts.jetstack.io
+#helm install cert-manager jetstack/cert-manager -n cert-manager --version v1.5.3
 
-# remove all stuff from minikube cluster
-kubectl delete namespace awx # daemonsets,replicasets,services,deployments,pods,rc --all
-kubectl get pods --no-headers=true --all-namespaces \
-   | sed -r 's/(\S+)\s+(\S+).*/kubectl --namespace \1 delete pod \2/e'
+## cron jobs
+## https://www.jeffgeerling.com/blog/2018/running-drupal-cron-jobs-kubernetes
+## https://www.youtube.com/watch?v=O1iEBzY7-ok
+
+## logging
+## injest into ELK? external provider with robust architecture
+
+## remove all stuff from minikube cluster
+kubectl delete -f awx-nginx.yml
+make undeploy
